1c4ffb0602538309ce4b849fbafb6fb3926eefa5
Nick Cruz <nickcruz920@gmail.com>
2022-01-10 15:24:50 -0500

    Made changes to README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Apartment Landing Page
+
+## Description: This is a simple landing page for a fictional Apartment Website
+
+## Technologies Used: HTML, CSS, Bootstrap, JavaScript, Chart.js, Intro.js
+
+### Author: Nick Cruz
---
+++ index.js
@@ -0,0 +1,72 @@
+// Form Validation 
+
+var forms = document.querySelectorAll('.needs-validation')
+Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+
+// Chart Section // 
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+        datasets: [{
+            label: 'Price Over Time',
+            data: [1080, 1220, 1170, 1140, 1050, 920],
+            borderColor: [
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+        datasets: [{
+            label: 'Price Over Time',
+            data: [980, 1040, 1140, 1270, 1380, 1520],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
